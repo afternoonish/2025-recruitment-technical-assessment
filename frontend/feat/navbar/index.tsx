54d363081f05ce0e80d081d977ee4414bf6698db
Jonathan Yeo <z5425103@ad.unsw.edu.au>
2025-02-27 19:38:25 +1100

    Remove template assets

--- frontend/feat/navbar/index.tsx
+++ frontend/feat/navbar/index.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
 export default function Navbar() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <div className="flex p-2 border-0 border-b-2 border-b-stone-200">
+        <Image src="/assets/freeRoomsLogo.png" alt="Freerooms logo" width={50} height={50} />
+      </div>
+    </nav>
+  );
 }
